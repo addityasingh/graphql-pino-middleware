090169555bb12e292de0c1cd029fce800d6ea8fa
Aditya Pratap Singh <adisinghrajput@gmail.com>
2020-01-28 23:26:17 +0100

    feat: Add tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,6 @@
+const pinoMock = require("pino");
 import graphqlPinoMiddleware, { LifecycleHook } from "..";
+jest.mock("pino");
 
 describe("graphql-pino-middleware", () => {
   const mockRoot = {};
@@ -6,7 +8,7 @@ describe("graphql-pino-middleware", () => {
   const mockInfo = {};
   const mockContext: any = {};
   const mockResolve = jest.fn();
-  
+
   beforeEach(() => {
     mockResolve.mockClear();
   });
@@ -21,5 +23,71 @@ describe("graphql-pino-middleware", () => {
       mockContext,
       mockInfo
     );
+  });
+
+  test("should add a default logger if no logger passed", async () => {
+    const mockResolve = jest.fn();
+    const middleware: any = graphqlPinoMiddleware();
+    await middleware(mockResolve, mockRoot, mockArgs, mockContext, mockInfo);
+    expect(mockResolve).toBeCalledWith(
+      mockRoot,
+      mockArgs,
+      mockContext,
+      mockInfo
+    );
+    expect(pinoMock).toHaveBeenCalled();
+  });
+
+  describe("hooks::using onion principle", () => {
+    test("should execute all `PreResolve` hooks", async () => {
+      const mockPreHook = jest.fn();
+      const mockHooks = {
+        [LifecycleHook.PreResolve]: [mockPreHook]
+      };
+
+      const middleware: any = graphqlPinoMiddleware(
+        { logger: pinoMock() },
+        mockHooks
+      );
+      await middleware(mockResolve, mockRoot, mockArgs, mockContext, mockInfo);
+      expect(mockPreHook).toBeCalled();
+      expect(mockPreHook).toBeCalledWith(mockContext);
+    });
+
+    test("should execute all `PostResolve` hooks", async () => {
+      const mockPostHook = jest.fn();
+      const mockHooks = {
+        [LifecycleHook.PostResolve]: [mockPostHook]
+      };
+
+      const middleware: any = graphqlPinoMiddleware(
+        { logger: pinoMock() },
+        mockHooks
+      );
+      await middleware(mockResolve, mockRoot, mockArgs, mockContext, mockInfo);
+      expect(mockPostHook).toBeCalled();
+      expect(mockPostHook).toBeCalledWith(mockContext);
+    });
+
+    test("should execute all hooks", async () => {
+      const mockPostHook = jest.fn();
+      const mockPreHook = jest.fn();
+      const mockResolve = jest.fn(() => "mock value");
+      const mockHooks = {
+        [LifecycleHook.PreResolve]: [mockPreHook],
+        [LifecycleHook.PostResolve]: [mockPostHook]
+      };
+
+      const middleware: any = graphqlPinoMiddleware(
+        { logger: pinoMock() },
+        mockHooks
+      );
+      await middleware(mockResolve, mockRoot, mockArgs, mockContext, mockInfo);
+      expect(mockPreHook).toBeCalled();
+      expect(mockPreHook).toBeCalledWith(mockContext);
+      expect(mockContext.resolvedData).toBe("mock value");
+      expect(mockPostHook).toBeCalled();
+      expect(mockPostHook).toBeCalledWith(mockContext);
+    });
   });
 });
